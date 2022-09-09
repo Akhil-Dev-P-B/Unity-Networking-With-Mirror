55bc671f53272db3acda23ecc9b34725d97f304b
Akhil Dev P B <akhildev1089@gmail.com>
2022-09-09 12:24:15 +0530

    Chapter 16. Spawning Units
      + Added new player prefab
    
      + Changed old player prefab to unit prefab
    
      + Added a material folder and a new material
    
      + Added Unit spawner prefab
    
      + Added building folder in sctipts folder
    
      + Added unit spawner script in building folder
    
      + Modified Network Manager

--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -5,5 +5,19 @@ using Mirror;
 
 public class RTSNetworkManager : NetworkManager
 {
-  
+
+    [SerializeField]
+    GameObject unitSpawner;
+
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        GameObject unitSpawnerInstance = Instantiate(
+            unitSpawner,
+            conn.identity.transform.position,
+            conn.identity.transform.rotation);
+        
+        NetworkServer.Spawn(unitSpawnerInstance,conn);
+    }
 }
